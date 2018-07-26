e8c598e7d8b076da71e8c46742e6e50045ba4f4a
perqin <perqinxie@gmail.com>
2018-07-26 21:18:29 +0800

    WIP: Add AMap Location SDK back

--- app/src/main/java/com/perqin/letmego/data/place/PlaceNotifier.kt
+++ app/src/main/java/com/perqin/letmego/data/place/PlaceNotifier.kt
@@ -1,9 +1,15 @@
 package com.perqin.letmego.data.place
 
+import android.annotation.SuppressLint
+import com.amap.api.location.AMapLocationClient
+
 /**
  * @author perqin
  */
 object PlaceNotifier {
+    @SuppressLint("StaticFieldLeak")
+    private var locationClient: AMapLocationClient? = null
+
     fun enableNotificationForPlace(place: Place) {}
 
     fun disableNotificationForPlace(place: Place) {}
